2c022033331a74b3738ae096cef69654c4237964
lucachaco <luiscarlosch@gmail.com>
2015-04-26 11:45:07 -0600

    Install node.

--- _conf/bootstrap.sh
+++ _conf/bootstrap.sh
@@ -10,6 +10,9 @@ dpkg-reconfigure locales
 
 apt-get install -y git
 
-apt-get install -y ruby ruby-dev make gcc nodejs
+apt-get install -y ruby ruby-dev make gcc
+
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+apt-get install -y nodejs
 
 gem install jekyll --no-rdoc --no-ri
