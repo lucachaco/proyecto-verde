d63f21f8c1c0cccfdcccaf58d15acc6c5ed80e41
lucachaco <luiscarlosch@gmail.com>
2015-04-26 12:31:36 -0600

    Gulp less working.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-    // place code for your default task here
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('less', function () {
+    return gulp.src('./assets/less/**/*.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('./public/css'));
 });
