0c5a72737cf4dfffd40de255116cddb89231003b
lucachaco <luiscarlosch@gmail.com>
2015-04-26 19:03:32 -0600

    Use custom bootstrap.

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,18 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
 
+gulp.task('bootstrap', function () {
+    return gulp.src('./assets/less/custom-bootstrap/custom-bootstrap.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('./assets/css/vendor'));
+});
+
 gulp.task('less', function () {
     return gulp.src('./assets/less/**/*.less')
         .pipe(less({
             paths: [ path.join(__dirname, 'less', 'includes') ]
         }))
-        .pipe(gulp.dest('./public/css'));
+        .pipe(gulp.dest('./assets/css'));
 });
